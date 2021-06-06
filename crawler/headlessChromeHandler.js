9b30f20f4339e6525426cdc047066e96887b89ab
이재훈 <hooney731@gmail.com>
2021-06-06 18:13:24 +0900

    review
    - semicolon(";") 다 붙여주세요.
    - 불필요한 주석 미리 제거해주세요.

--- crawler/headlessChromeHandler.js
+++ crawler/headlessChromeHandler.js
@@ -8,43 +8,38 @@ async function automaticChromeHandler(browser, JSONconfig, targetDate) {
     let previousPageItemCount = -1;
     let currentPageItemCount = -1;
 
-    // pipe page's console data to terminal
-    // page.on('console', consoleObj => console.log(consoleObj.text()));
-
     while((previousPageItemCount==currentPageItemCount) || (previousPageItemCount === -1)){
         previousPageItemCount = currentPageItemCount;
         let targetDateSearchPage = JSONconfig["searchPageBaseURL"] + pageNum  
                 + JSONconfig["searchPageTargetDateStart"] + dateFormatParse.format(targetDate, JSONconfig["searchDateFormatStart"])
                 + JSONconfig["searchPageTargetDateEnd"] + dateFormatParse.format(targetDate, JSONconfig["searchDateFormatEnd"])
-        console.log(`go to ${targetDateSearchPage}`)
+        console.log(`go to ${targetDateSearchPage}`);
         await page.goto(targetDateSearchPage, { waitUntil: 'networkidle0' })
         .catch(e=>{
             console.log(e);
         })
-        let data = await htmlHandler.getUrlsOnSearchPage(page, JSONconfig["searchPagePostURLSelector"], JSONconfig["source"])
-        currentPageItemCount = data.length
+        let data = await htmlHandler.getUrlsOnSearchPage(page, JSONconfig["searchPagePostURLSelector"], JSONconfig["source"]);
+        currentPageItemCount = data.length;
     
         // get actual data from post with scraped urls
         for (let idx = 0; idx < currentPageItemCount; idx++) {
-            console.log(`go to ${data[idx]["herf"]}`)
-            await page.goto(data[idx]["herf"], { waitUntil: 'networkidle0' })
+            console.log(`go to ${data[idx]["herf"]}`);
+            await page.goto(data[idx]["herf"], { waitUntil: 'networkidle0' });
             let frame;
             if (JSONconfig["innerIframeId"] !== "") {
-                frame = page.frames().find(frame => frame.name() === JSONconfig["innerIframeId"])
+                frame = page.frames().find(frame => frame.name() === JSONconfig["innerIframeId"]);
             }
             else {
                 frame = page;
             }
     
-            const returnedActualPostData = await htmlHandler.postContentsParser(frame, JSONconfig["postSelectorData"])
+            const returnedActualPostData = await htmlHandler.postContentsParser(frame, JSONconfig["postSelectorData"]);
     
-            data[idx]["postData"] = {};
-            data[idx]["postData"] = returnedActualPostData;
-            // console.log(data[idx]["postData"])
+            data[idx]["postData"] = {returnedActualPostData};
             
             
         }
-        db.putData(data)
+        db.putData(data);
  
         pageNum += 1;
     }
